b1b0dd242e0e1f1cf38e06afa4ca61499ecb8d58
KePatrick <waspat1109@yahoo.com>
2023-12-24 01:27:47 +0800

    12/24

---
+++ demo/src/main/java/com/example/demo/OrderController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.service.OrderService;
+
+
+
+@RestController
+public class OrderController {
+	
+	@Autowired
+	OrderService orderService;
+
+	@PostMapping("/ecpayCheckout")
+	public String ecpayCheckout() {
+		String aioCheckOutALLForm = orderService.ecpayCheckout();
+		
+		return aioCheckOutALLForm;
+	}
+}
